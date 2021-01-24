cc12ba045f8de1ec022027b74134512dc1085fbb
Cristhian O <cristhian.ov2@gmail.com>
2021-01-24 10:03:21 -0500

    Second option

--- app.js
+++ app.js
@@ -59,4 +59,7 @@ app.use("*", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
+app.get('/', (req, res) => {
+  res.redirect('/api');
+});
 module.exports = app;
